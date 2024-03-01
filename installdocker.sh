01afe21829f8df2a8293ba97a22570037bfde9cf
ntd284 <nguyentuanduong7@example.com>
2024-03-01 10:51:38 +0700

    update git

--- installdocker.sh
+++ installdocker.sh
@@ -5,3 +5,5 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
 sudo apt install docker-ce docker-compose-plugin
+sudo usermod -aG docker ${USER}
+sudo su - ${USER}
